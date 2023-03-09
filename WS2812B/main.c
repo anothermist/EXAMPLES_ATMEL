0ffedb632d62f308faac26d502806178d02ae0d0
unknown <C:\Users\USER\AppData\Roaming\The Bat!>
2023-03-10 02:09:27 +0500

    HSVToRGB

--- WS2812B/main.c
+++ WS2812B/main.c
@@ -2,7 +2,91 @@
 
 struct cRGB led[40];
 
+struct RGB { unsigned char R; unsigned char G; unsigned char B; };
+struct HSV { double H; double S; double V; };
+
+struct RGB HSVToRGB(struct HSV hsv) {
+	double r = 0, g = 0, b = 0;
+
+	if (hsv.S == 0) {
+		r = hsv.V;
+		g = hsv.V;
+		b = hsv.V;
+	}
+	else {
+		int i;
+		double f, p, q, t;
+
+		if (hsv.H == 360)
+		hsv.H = 0;
+		else
+		hsv.H = hsv.H / 60;
+
+		i = (int)trunc(hsv.H);
+		f = hsv.H - i;
+
+		p = hsv.V * (1.0 - hsv.S);
+		q = hsv.V * (1.0 - (hsv.S * f));
+		t = hsv.V * (1.0 - (hsv.S * (1.0 - f)));
+
+		switch (i) {
+			case 0:
+			r = hsv.V;
+			g = t;
+			b = p;
+			break;
+
+			case 1:
+			r = q;
+			g = hsv.V;
+			b = p;
+			break;
+
+			case 2:
+			r = p;
+			g = hsv.V;
+			b = t;
+			break;
+
+			case 3:
+			r = p;
+			g = q;
+			b = hsv.V;
+			break;
+
+			case 4:
+			r = t;
+			g = p;
+			b = hsv.V;
+			break;
+
+			default:
+			r = hsv.V;
+			g = p;
+			b = q;
+			break;
+		}
+
+	}
+
+	struct RGB rgb;
+	rgb.R = r * 255;
+	rgb.G = g * 255;
+	rgb.B = b * 255;
+
+	return rgb;
+}
+
 int main(void) {
+	
+	for (int i = 0; i < 40; i++) {
+		struct HSV data = { i*(360/40), 1, 0.02 };
+		struct RGB value = HSVToRGB(data);
+		led[i].r=value.R; led[i].g=value.G; led[i].b=value.B;
+	}
+	ws2812_setleds(led, 40);
+	
+	_delay_ms(2000);
 
 	uint8_t pos=0;
 	uint8_t direction=1;
