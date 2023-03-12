c26574060468994553ec915bee16b2f15cda54e3
unknown <C:\Users\USER\AppData\Roaming\The Bat!>
2023-03-12 15:39:19 +0500

    Simplified

--- WS2812B/main.c
+++ WS2812B/main.c
@@ -5,27 +5,21 @@
 #define brightness 0.03  // 0.00 ~ 1.00
 
 struct cRGB led[length];
+
 struct RGB { uint8_t R; uint8_t G; uint8_t B; };
 struct HSV { double H; double S; double V; };
 
 struct RGB HSVToRGB(struct HSV hsv) {
 	double r = 0, g = 0, b = 0;
 
-	if (hsv.S == 0) {
-		r = hsv.V;
-		g = hsv.V;
-		b = hsv.V;
-	}
+	if (hsv.S == 0) { r = hsv.V; g = hsv.V; b = hsv.V; }
 	else {
-		int i;
+		uint16_t i;
 		double f, p, q, t;
 
-		if (hsv.H == 360)
-		hsv.H = 0;
-		else
-		hsv.H = hsv.H / 60;
+		if (hsv.H == 360) hsv.H = 0; else hsv.H = hsv.H / 60;
 
-		i = (int)trunc(hsv.H);
+		i = (uint16_t)trunc(hsv.H);
 		f = hsv.H - i;
 
 		p = hsv.V * (1.0 - hsv.S);
@@ -33,50 +27,17 @@ struct RGB HSVToRGB(struct HSV hsv) {
 		t = hsv.V * (1.0 - (hsv.S * (1.0 - f)));
 
 		switch (i) {
-			case 0:
-			r = hsv.V;
-			g = t;
-			b = p;
-			break;
-
-			case 1:
-			r = q;
-			g = hsv.V;
-			b = p;
-			break;
-
-			case 2:
-			r = p;
-			g = hsv.V;
-			b = t;
-			break;
-
-			case 3:
-			r = p;
-			g = q;
-			b = hsv.V;
-			break;
-
-			case 4:
-			r = t;
-			g = p;
-			b = hsv.V;
-			break;
-
-			default:
-			r = hsv.V;
-			g = p;
-			b = q;
-			break;
+			case 0:  r = hsv.V; g = t;     b = p;     break;
+			case 1:  r = q;     g = hsv.V; b = p;     break;
+			case 2:  r = p;     g = hsv.V; b = t;     break;
+			case 3:  r = p;     g = q;     b = hsv.V; break;
+			case 4:  r = t;     g = p;     b = hsv.V; break;
+			default: r = hsv.V; g = p;     b = q;     break;
 		}
 
 	}
-
 	struct RGB rgb;
-	rgb.R = r * 255;
-	rgb.G = g * 255;
-	rgb.B = b * 255;
-
+	rgb.R = r * 255; rgb.G = g * 255; rgb.B = b * 255;
 	return rgb;
 }
 
@@ -92,47 +53,43 @@ int main(void) {
 	//int8_t direction = 1;
 	uint16_t position = 0;
 	
-	for (uint16_t i = 0; i < length; i++) {
-		struct HSV data = { i*(360/length), saturation, brightness };
-		struct RGB value = HSVToRGB(data);
-		led[i].r=value.R; led[i].g=value.G; led[i].b=value.B;
-	}
+	//for (uint16_t i = 0; i < length; i++) {
+		//struct HSV data = { i*(360/length), saturation, brightness };
+		//struct RGB value = HSVToRGB(data);
+		//led[i].r=value.R; led[i].g=value.G; led[i].b=value.B;
+	//}
 	
 	while(1) {
 		
 		struct HSV data = { position*(360/(length)), saturation, brightness };
 		struct RGB value = HSVToRGB(data);
 		
-		uint8_t tempR = value.R; // led[0].r;
-		uint8_t tempG = value.G; // led[0].g;
-		uint8_t tempB = value.B; // led[0].b;
-		
 		for (uint16_t i = 0; i < (length - 1); i++) {
 			led[i].r = led[i + 1].r;
 			led[i].g = led[i + 1].g;
 			led[i].b = led[i + 1].b;
 		}
-		led[length - 1].r = tempR;
-		led[length - 1].g = tempG;
-		led[length - 1].b = tempB;
+		led[length - 1].r = value.R;
+		led[length - 1].g = value.G;
+		led[length - 1].b = value.B;
 		
 		ws2812_setleds(led, length);
 		position++;
 		if (position == length) position = 0;
 		_delay_ms(move_delay);
+		
+		//led[0].r=255; led[0].g=0; led[0].b=0;
+		//led[1].r=0;   led[1].g=0; led[1].b=255;
+		//
+		//for (uint16_t i = 0; i < position; i++)
+		//ws2812_send((uint8_t *)&led[0]);
+		//
+		//for (uint16_t i = 0; i < (length - position); i++)
+		//ws2812_send((uint8_t *)&led[1]);
+		//
+		//position+=direction;
+		//if ((position == length)||(position == 0)) direction =- direction;
+		//_delay_ms(move_delay);
+		
 	}
-	
-	//led[0].r=255; led[0].g=0; led[0].b=0;
-	//led[1].r=0;   led[1].g=0; led[1].b=255;
-	//
-	//for (uint16_t i = 0; i < position; i++)
-	//ws2812_send((uint8_t *)&led[0]);
-	//
-	//for (uint16_t i = 0; i < (length - position); i++)
-	//ws2812_send((uint8_t *)&led[1]);
-	//
-	//position+=direction;
-	//if ((position == length)||(position == 0)) direction =- direction;
-	//_delay_ms(move_delay);
-	//}
 }
